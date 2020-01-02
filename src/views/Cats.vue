206dae383aeb53da2d26559702d1aa86d764e60b
cetric <cetric32@gmail.com>
2020-01-02 21:40:02 +0300

    somehow
     Changes to be committed:
    	new file:   public/bootstrap/css/bootstrap-grid.css
    	new file:   public/bootstrap/css/bootstrap-grid.css.map
    	new file:   public/bootstrap/css/bootstrap-grid.min.css
    	new file:   public/bootstrap/css/bootstrap-grid.min.css.map
    	new file:   public/bootstrap/css/bootstrap-reboot.css
    	new file:   public/bootstrap/css/bootstrap-reboot.css.map
    	new file:   public/bootstrap/css/bootstrap-reboot.min.css
    	new file:   public/bootstrap/css/bootstrap-reboot.min.css.map
    	new file:   public/bootstrap/css/bootstrap.css
    	new file:   public/bootstrap/css/bootstrap.css.map
    	new file:   public/bootstrap/css/bootstrap.min.css
    	new file:   public/bootstrap/css/bootstrap.min.css.map
    	new file:   public/bootstrap/js/bootstrap.bundle.js
    	new file:   public/bootstrap/js/bootstrap.bundle.js.map

---
+++ src/views/Cats.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Cats for Adoption</h1>
+    <table class="table table-striped table-dark table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Breed</th>
+          <th scope="col">Gender</th>
+          <th scope="col">Age</th>
+          <th scope="col">Color</th>
+          <th scope="col">Weight</th>
+          <th scope="col">Location</th>
+          <th scope="col">Notes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(cat,index) in cats" :key="index">
+          <th scope="row">{{index}}</th>
+          <td>
+            <router-link :to="`/pets/cats/${index}`"> {{cat.name}}</router-link>
+          </td>
+          <td>{{cat.breed}}</td>
+          <td>{{cat.gender}}</td>
+          <td>{{cat.age}}</td>
+          <td>{{cat.color}}</td>
+          <td>{{cat.weight}}</td>
+          <td>{{cat.location}}</td>
+          <td>{{cat.notes}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Cats',
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Dogs.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>Dogs for Adoption</h1>
+    <table class="table table-striped table-dark table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Breed</th>
+          <th scope="col">Gender</th>
+          <th scope="col">Age</th>
+          <th scope="col">Color</th>
+          <th scope="col">Weight</th>
+          <th scope="col">Lodogion</th>
+          <th scope="col">Notes</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(dog,index) in dogs" :key="index">
+          <th scope="row">{{index}}</th>
+          <td>
+            <router-link :to="`/pets/dogs/${index}`"> {{dog.name}}</router-link>
+          </td>
+          <td>{{dog.breed}}</td>
+          <td>{{dog.gender}}</td>
+          <td>{{dog.age}}</td>
+          <td>{{dog.color}}</td>
+          <td>{{dog.weight}}</td>
+          <td>{{dog.lodogion}}</td>
+          <td>{{dog.notes}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Dogs',
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'dogs'
+    ])
+  }
+}
+</script>
+
+<style>
+
+</style>
